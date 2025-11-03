c4ac84e76dfa9459d232b3699971e5e7e38e8731
unknown <hoangnguyen164@gmail.com>
2025-11-04 02:12:31 +0700

    first commit

---
+++ src/main/java/com/hn369/ecommerce/Hn369EcommerceDbManagementApplication.java
@@ -0,0 +1,11 @@
+package com.hn369.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hn369EcommerceDbManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Hn369EcommerceDbManagementApplication.class, args);
+    }
+}
